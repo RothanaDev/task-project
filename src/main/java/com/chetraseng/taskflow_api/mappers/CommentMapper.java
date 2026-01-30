6cb3bbcd68067f76c622195bde40fbef6573c36c
Chetra Seng <dev@chetraseng.com>
2026-01-30 22:59:55 +0700

    fix missing component model

--- src/main/java/com/chetraseng/taskflow_api/mappers/CommentMapper.java
+++ src/main/java/com/chetraseng/taskflow_api/mappers/CommentMapper.java
@@ -1,11 +1,12 @@
 package com.chetraseng.taskflow_api.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants.ComponentModel;
 
 import com.chetraseng.taskflow_api.dto.responses.CommentResponse;
 import com.chetraseng.taskflow_api.models.CommentModel;
 
-@Mapper
+@Mapper(componentModel = ComponentModel.SPRING)
 public interface CommentMapper {
   CommentResponse toCommentResponse(CommentModel comment);
 }
--- src/main/java/com/chetraseng/taskflow_api/mappers/TaskMapper.java
+++ src/main/java/com/chetraseng/taskflow_api/mappers/TaskMapper.java
@@ -2,11 +2,12 @@ package com.chetraseng.taskflow_api.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants.ComponentModel;
 
 import com.chetraseng.taskflow_api.dto.responses.TaskListResponse;
 import com.chetraseng.taskflow_api.models.TaskModel;
 
-@Mapper
+@Mapper(componentModel = ComponentModel.SPRING)
 public interface TaskMapper {
   @Mapping(target = "projectName", source = "project.name")
   @Mapping(target = "comment", expression = "java(task.getComments().size())")
